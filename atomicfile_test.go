ecfbd53ceeb92b297d9f5c8dcc0979b31f1c361e
Naitik Shah <n@daaku.org>
2013-02-21 15:50:25 -0800

    also test with abs path and dir component

--- atomicfile_test.go
+++ atomicfile_test.go
@@ -7,20 +7,22 @@ import (
 )
 
 func TestSimple(t *testing.T) {
-	const name = "TestSimple"
-	defer os.Remove(name)
-	f, err := atomicfile.New(name)
-	if err != nil {
-		t.Fatal(err)
-	}
-	f.Write([]byte("foo"))
-	if _, err := os.Stat(name); !os.IsNotExist(err) {
-		t.Fatal("did not expect file to exist")
-	}
-	if err := f.Close(); err != nil {
-		t.Fatal(err)
-	}
-	if _, err := os.Stat(name); err != nil {
-		t.Fatalf("expected file to exist: %s", err)
+	names := []string{"TestSimple", "/tmp/foo"}
+	for _, name := range names {
+		defer os.Remove(name)
+		f, err := atomicfile.New(name)
+		if err != nil {
+			t.Fatal(err)
+		}
+		f.Write([]byte("foo"))
+		if _, err := os.Stat(name); !os.IsNotExist(err) {
+			t.Fatal("did not expect file to exist")
+		}
+		if err := f.Close(); err != nil {
+			t.Fatal(err)
+		}
+		if _, err := os.Stat(name); err != nil {
+			t.Fatalf("expected file to exist: %s", err)
+		}
 	}
 }
